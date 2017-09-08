64f6241c2b10762ca062fc78b72d7dede11fb96d
ctdoan33 <doanchristopher_1@yahoo.com>
2017-09-08 12:03:24 -0700

    completed hacker, accepts uppercase

--- server.py
+++ server.py
@@ -3,16 +3,23 @@ app = Flask(__name__)
 @app.route("/")
 def choose():
     return render_template("index.html")
-@app.route("/result", methods=["POST"])
+@app.route("/request", methods=["POST"])
 def ninja():
-    color = request.form["color"]
+    color = request.form["color"].lower()
     if color == "red":
         src="../static/raphael.jpg"
+        name="You chose Raphael!"
     elif color == "blue":
         src="../static/leonardo.jpg"
+        name="You chose Leonardo!"
     elif color == "orange":
         src="../static/michelangelo.jpg"
+        name="You chose Michelangelo!"
     elif color == "purple":
         src="../static/donatello.jpg"
-    return src
+        name="You chose Donatello!"
+    else:
+        src="../static/notapril.jpg"
+        name="There's no ninja in that color!"
+    return jsonify(name=name,source=src)
 app.run(debug=True)
